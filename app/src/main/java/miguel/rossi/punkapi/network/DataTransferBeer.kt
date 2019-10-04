867ac42b314768573d0b02169d9662992f9c97cf
MiguelRossi <rossisanahuja@gmail.com>
2019-10-04 09:10:05 +0100

    Change DataTransferBeer asDomainModel() to asEntity()

--- app/src/main/java/miguel/rossi/punkapi/network/DataTransferBeer.kt
+++ app/src/main/java/miguel/rossi/punkapi/network/DataTransferBeer.kt
@@ -2,7 +2,9 @@ package miguel.rossi.punkapi.network
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import miguel.rossi.punkapi.domain.*
+import miguel.rossi.punkapi.database.CellarEntity
+import miguel.rossi.punkapi.database.DatabaseBeer
+import miguel.rossi.punkapi.database.DatabaseFood
 
 @JsonClass(generateAdapter = true)
 data class NetworkBeer(
@@ -15,25 +17,41 @@ data class NetworkBeer(
     val imageUrl: String?,
     val abv: Double?,
     @Json(name = "food_pairing")
-    val foodPairing: List<String>?,
+    val foodPairing: List<String?>?,
     @Json(name = "brewers_tips")
     val brewersTips: String?,
     @Json(name = "contributed_by")
     val contributedBy: String?
 )
 
-fun List<NetworkBeer>.asDomainModel(): List<Beer> {
-    return this.map {
-        Beer(
+fun List<NetworkBeer>.asEntity(): CellarEntity {
+    val listDatabaseBeer = getDatabaseBeerList(this)
+    val listDatabaseFood = getDatabaseFoodList(this)
+    return CellarEntity(listDatabaseBeer, listDatabaseFood)
+}
+
+private fun getDatabaseBeerList(list: List<NetworkBeer>) =
+    list.map {
+        DatabaseBeer(
             id = it.id,
             name = it.name ?: "",
             tagLine = it.tagLine ?: "",
             description = it.description ?: "",
             imageUrl = it.imageUrl ?: "",
             abv = it.abv ?: 0.0,
-            foodPairing = it.foodPairing ?: listOf(),
             brewersTips = it.brewersTips ?: "",
             contributedBy = it.contributedBy ?: ""
         )
     }
+
+private fun getDatabaseFoodList(list: List<NetworkBeer>): List<DatabaseFood> {
+    val databaseFoodList = mutableListOf<DatabaseFood>()
+    for (i in list.indices) {
+        val loops = list[i].foodPairing?.size ?: 0
+        for (j in 0 until loops) {
+            val food: String = list[i].foodPairing!![j] ?: ""
+            databaseFoodList.add(DatabaseFood(id = j, food = food, beerId = list[i].id))
+        }
+    }
+    return databaseFoodList
 }
--- app/src/test/java/miguel/rossi/punkapi/network/DataTransferBeerTest.kt
+++ app/src/test/java/miguel/rossi/punkapi/network/DataTransferBeerTest.kt
@@ -1,26 +1,42 @@
 package miguel.rossi.punkapi.network
 
 import com.google.common.truth.Truth.assertThat
-import miguel.rossi.punkapi.utils.getBeer
-import miguel.rossi.punkapi.utils.getNetworkBeer
+import miguel.rossi.punkapi.database.DatabaseFood
+import miguel.rossi.punkapi.utils.getCellarEntity
+import miguel.rossi.punkapi.utils.getEmptyDatabaseBeer
+import miguel.rossi.punkapi.utils.getEmptyNetworkBeer
+import miguel.rossi.punkapi.utils.getNetworkBeerList
 import org.junit.Test
 
 class DataTransferBeerTest {
 
     @Test
-    fun asDomainModel_whenAllNull_thenAllDefaultValues() {
-        val listOfNetworkBeer = listOf(getNetworkBeer(null))
+    fun asEntity_whenEmptyList_thenAllDefaultValues() {
+        val listOfNetworkBeer = listOf<NetworkBeer>()
+        val entity = listOfNetworkBeer.asEntity()
 
-        assertThat(listOfNetworkBeer.size).isEqualTo(1)
-        assertThat(listOfNetworkBeer.asDomainModel()).isEqualTo(listOf(getBeer(null)))
+        assertThat(entity.databaseBeer.size).isEqualTo(listOfNetworkBeer.size)
     }
 
     @Test
-    fun asDomainModel_whenNoneNull_thenAllGivenValues() {
-        val listOfNetworkBeer = listOf(getNetworkBeer(10))
+    fun asEntity_whenNull_thenDefaultValues() {
+        val listOfNetworkBeer = getEmptyNetworkBeer()
+        val entity = listOfNetworkBeer.asEntity()
 
         assertThat(listOfNetworkBeer.size).isEqualTo(1)
-        assertThat(listOfNetworkBeer.asDomainModel()).isEqualTo(listOf(getBeer(10)))
+        assertThat(entity.databaseFood).containsExactlyElementsIn(listOf<DatabaseFood>())
+        assertThat(entity.databaseBeer).containsExactlyElementsIn(getEmptyDatabaseBeer())
+    }
+
+    @Test
+    fun asEntity_whenNoneNull_thenAllGivenValues() {
+        val listOfNetworkBeer = getNetworkBeerList()
+        val entity = listOfNetworkBeer.asEntity()
+        val cellarEntity = getCellarEntity()
+
+        assertThat(listOfNetworkBeer.size).isEqualTo(cellarEntity.databaseBeer.size)
+        assertThat(entity.databaseBeer).containsExactlyElementsIn(cellarEntity.databaseBeer)
+        assertThat(entity.databaseFood).containsExactlyElementsIn(cellarEntity.databaseFood)
     }
 
 }
