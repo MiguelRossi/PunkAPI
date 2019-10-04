bf75a92026f1931fae3a00f1c747b3d201938301
MiguelRossi <rossisanahuja@gmail.com>
2019-10-04 10:24:34 +0100

    Add more cases to the test BeerUtil

--- app/src/sharedTest/java/miguel/rossi/punkapi/utils/BeerUtil.kt
+++ app/src/sharedTest/java/miguel/rossi/punkapi/utils/BeerUtil.kt
@@ -9,13 +9,13 @@ import miguel.rossi.punkapi.network.NetworkBeer
 fun getBeerList() = listOf(
     Beer(0, "0", "0", "0", "0", 0.0, listOf("foodPairing_0", "foodPairing_1"), "0", "0"),
     Beer(1, "1", "1", "1", "1", 1.0, listOf("foodPairing_1", "foodPairing_2"), "1", "1"),
-    Beer(2, "2", "2", "2", "2", 2.0, listOf("foodPairing_2", "foodPairing_3"), "2", "2"),
+    Beer(2, "2", "2", "2", "2", 2.0, listOf("", "foodPairing_3"), "2", "2"),
     Beer(3, "3", "3", "3", "3", 3.0, listOf("foodPairing_3", "foodPairing_4"), "3", "3"),
-    Beer(4, "4", "4", "4", "4", 4.0, listOf("foodPairing_4", "foodPairing_5"), "4", "4"),
-    Beer(5, "5", "5", "5", "5", 5.0, listOf("foodPairing_5", "foodPairing_6"), "5", "5"),
+    Beer(4, "4", "4", "4", "4", 4.0, listOf("foodPairing_4", ""), "4", "4"),
+    Beer(5, "5", "5", "5", "5", 5.0, listOf("", ""), "5", "5"),
     Beer(6, "6", "6", "6", "6", 6.0, listOf("foodPairing_6", "foodPairing_7"), "6", "6"),
     Beer(7, "7", "7", "7", "7", 7.0, listOf("foodPairing_7", "foodPairing_8"), "7", "7"),
-    Beer(8, "8", "8", "8", "8", 8.0, listOf("foodPairing_8", "foodPairing_9"), "8", "8"),
+    Beer(8, "8", "8", "8", "8", 8.0, listOf("", ""), "8", "8"),
     Beer(9, "9", "9", "9", "9", 9.0, listOf("foodPairing_9", "foodPairing_0"), "9", "9")
 )
 
@@ -24,13 +24,13 @@ fun getEmptyNetworkBeer() = listOf(NetworkBeer(0, null, null, null, null, null, 
 fun getNetworkBeerList() = listOf(
     NetworkBeer(0, "0", "0", "0", "0", 0.0, listOf("foodPairing_0", "foodPairing_1"), "0", "0"),
     NetworkBeer(1, "1", "1", "1", "1", 1.0, listOf("foodPairing_1", "foodPairing_2"), "1", "1"),
-    NetworkBeer(2, "2", "2", "2", "2", 2.0, listOf("foodPairing_2", "foodPairing_3"), "2", "2"),
+    NetworkBeer(2, "2", "2", "2", "2", 2.0, listOf("", "foodPairing_3"), "2", "2"),
     NetworkBeer(3, "3", "3", "3", "3", 3.0, listOf("foodPairing_3", "foodPairing_4"), "3", "3"),
-    NetworkBeer(4, "4", "4", "4", "4", 4.0, listOf("foodPairing_4", "foodPairing_5"), "4", "4"),
-    NetworkBeer(5, "5", "5", "5", "5", 5.0, listOf("foodPairing_5", "foodPairing_6"), "5", "5"),
+    NetworkBeer(4, "4", "4", "4", "4", 4.0, listOf("foodPairing_4", ""), "4", "4"),
+    NetworkBeer(5, "5", "5", "5", "5", 5.0, listOf("", ""), "5", "5"),
     NetworkBeer(6, "6", "6", "6", "6", 6.0, listOf("foodPairing_6", "foodPairing_7"), "6", "6"),
     NetworkBeer(7, "7", "7", "7", "7", 7.0, listOf("foodPairing_7", "foodPairing_8"), "7", "7"),
-    NetworkBeer(8, "8", "8", "8", "8", 8.0, listOf("foodPairing_8", "foodPairing_9"), "8", "8"),
+    NetworkBeer(8, "8", "8", "8", "8", 8.0, listOf("", ""), "8", "8"),
     NetworkBeer(9, "9", "9", "9", "9", 9.0, listOf("foodPairing_9", "foodPairing_0"), "9", "9")
 )
 
@@ -60,20 +60,20 @@ fun getDatabaseFoodList() = listOf(
     DatabaseFood(1, "foodPairing_1", 0),
     DatabaseFood(0, "foodPairing_1", 1),
     DatabaseFood(1, "foodPairing_2", 1),
-    DatabaseFood(0, "foodPairing_2", 2),
+    DatabaseFood(0, "", 2),
     DatabaseFood(1, "foodPairing_3", 2),
     DatabaseFood(0, "foodPairing_3", 3),
     DatabaseFood(1, "foodPairing_4", 3),
     DatabaseFood(0, "foodPairing_4", 4),
-    DatabaseFood(1, "foodPairing_5", 4),
-    DatabaseFood(0, "foodPairing_5", 5),
-    DatabaseFood(1, "foodPairing_6", 5),
+    DatabaseFood(1, "", 4),
+    DatabaseFood(0, "", 5),
+    DatabaseFood(1, "", 5),
     DatabaseFood(0, "foodPairing_6", 6),
     DatabaseFood(1, "foodPairing_7", 6),
     DatabaseFood(0, "foodPairing_7", 7),
     DatabaseFood(1, "foodPairing_8", 7),
-    DatabaseFood(0, "foodPairing_8", 8),
-    DatabaseFood(1, "foodPairing_9", 8),
+    DatabaseFood(0, "", 8),
+    DatabaseFood(1, "", 8),
     DatabaseFood(0, "foodPairing_9", 9),
     DatabaseFood(1, "foodPairing_0", 9)
 )
